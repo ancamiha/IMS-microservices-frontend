43b111cda76f5c9cf9b6abde81c8ba369a5c10f1
Anca Enache <anca_mihaela.enache@stud.acs.upb.com>
2024-01-16 00:53:41 +0200

    add Order btn

--- sweet-shop/src/pages/CartContent.jsx
+++ sweet-shop/src/pages/CartContent.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import CartList from '../components/CartList';
+import { useNavigate } from "react-router-dom";
 
 function CartContent() {
+  const navigate = useNavigate();
+
+  const handleOrderClick = () => {
+    navigate('/order');
+  };
+
   return (
     <div>
         <CartList />
+        <div className='container'>
+          <button id="orderBtn" className="buttonStyle" onClick={handleOrderClick}>Order</button>
+        </div>
     </div>
   );
 }
